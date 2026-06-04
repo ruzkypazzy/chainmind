01614271edd6ef90509549255d056250b7604e31
ruzkypazzy <78404403+ruzkypazzy@users.noreply.github.com>
2026-06-04 06:15:56 +0100

    Drop dead ChainConnectors import; re-export PharosConnectors

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,22 @@
-from .chain_connectors import ChainConnectors
+"""
+utils/__init__.py
+
+Re-exports the utility helpers used by the modules.
+"""
+from .chain_connectors import (
+    PharosConnectors,
+    ERC20_TRANSFER_TOPIC,
+    SELECTOR_ERC20_TRANSFER,
+    SELECTOR_ERC20_APPROVAL,
+)
 from .data_formatters import DataFormatters
 from .cache_manager import CacheManager
 
-__all__ = ["ChainConnectors", "DataFormatters", "CacheManager"]
+__all__ = [
+    "PharosConnectors",
+    "ERC20_TRANSFER_TOPIC",
+    "SELECTOR_ERC20_TRANSFER",
+    "SELECTOR_ERC20_APPROVAL",
+    "DataFormatters",
+    "CacheManager",
+]
